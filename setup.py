3e800bdb1b5559b18f9035ecdcb01a1ee3867a6b
Petr Tobiska <petr.tobiska@gmail.com>
2016-02-02 16:05:34 +0100

    correction of setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -14,25 +14,27 @@ setup(name='asterix',
 asterix is a framework for communication with smartcards based on pyscard
 
 It consists of
+ mycard        - a module providing handy primitives for card
+                 communication (extensions to pyscard)
  formutil      - a module with formatting utilities
- GAF           - a module for representation and evaluation of ASN1 structures
- mycard        - a module providing handy primitives for card communication
- APDU          - a module for creation and transmission of common APDU to
- 	         a smartcard
- SCP02         - implementation of Secure Channel Protocol 02 as defined in
- 	         Global platform
- SCP03         - implementation of Secure Channel Protocol 03 as defined in
- 	         Global platform
- applet        - a module for loading, installation and deletion of JavaCard
- 	         applets to a smartcard
- CAT           - a module implementing Card Application Toolkit as defined in
-                 ETSI TS 102.223
- SecurePacket - a module implementing Secure Packet for OTA communication
- 	         with smartcards
+ GAF           - a module for representation and evaluation of ASN1
+                 structures
+ APDU          - a module for creation and transmission of common
+                 APDU to a smartcard
+ SCP02         - implementation of Secure Channel Protocol 02 as
+                 defined in Global platform
+ SCP03         - implementation of Secure Channel Protocol 03 as
+                 defined in Global platform
+ applet        - a module for loading, installation and deletion
+                 of JavaCard applets to a smartcard
+ CAT           - a module implementing Card Application Toolkit as
+                 defined in ETSI TS 102.223
+ SecurePacket  - a module implementing Secure Packet for OTA
+                 communication with smartcards
 
 All modules are platform independent, working with Python 2.7
 
-All modules relies on pyscard, a package implementing PCSC layer for
-communication with smartcards.
+All modules rely on pyscard, a package implementing PCSC layer
+for communication with smartcards.
 """,
      )
